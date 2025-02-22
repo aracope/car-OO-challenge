039ba2fcdcb568402efabb44ba57079a4d506453
aracope <156398622+aracope@users.noreply.github.com>
2025-02-21 21:24:36 -0800

    Garage class successfully tested.

--- script.js
+++ script.js
@@ -80,3 +80,20 @@ class Motorcycle extends Vehicle {
 
 // garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
 // "Sorry, we're full."
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+  add(newVehicle) {
+    if(!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if(this.vehicles.length >= this.capacity) {
+        return "Sorry, we're full."
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
+}
